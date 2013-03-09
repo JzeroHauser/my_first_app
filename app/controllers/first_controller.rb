e1f56d1e8b92706dc0f8afc07fc2e71aa3c4fb3d
Jzero Hauser <jhauser40@academic.rrc.ca>
2013-03-09 10:49:15 -0600

    initial commit

---
+++ app/controllers/first_controller.rb
@@ -0,0 +1,15 @@
+class FirstController < ApplicationController
+	#This is an action called index.
+	def index
+  end
+  # Rails will now automatically load up the view of the same name
+  # from the app/views/first folder: index.html.erb.
+  def cats
+
+  end
+  def about_us
+    @title = 'About Us'
+    @content = 'This company was built on blood sweat and tears.'
+  end    #autotmatically load: app/views/first/about_us.html.erb
+         #this view will have access to the @title and @content variables
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyFirstApp::Application.config.secret_token = 'a35f302b95939cea88a5c2de2671c7e92426160987f76e64ce4c003032aa9657b8036fd7710e413ffdff05a99ef34304ffa4f6a176bcb2ce68f17fa9fd9036ae'
